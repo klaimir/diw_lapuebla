0917a5dda3e896486eac06dccbee687bd0120181
Ángel Berasuain <angel.berasuain@gmail.com>
2026-01-20 19:01:56 +0100

    Ejemplos de GRID y Modo claro/oscuro

---
+++ UD6/theme_schema/funciones.js
@@ -0,0 +1,9 @@
+const toggleButton = document.getElementById('toggleMode');
+
+toggleButton.addEventListener('click', () => {
+    if (document.body.getAttribute('data-theme') === 'dark') {
+        document.body.removeAttribute('data-theme');
+    } else {
+        document.body.setAttribute('data-theme', 'dark');
+    }
+});
